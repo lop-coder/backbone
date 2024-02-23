515ad7a51c2fb397001b19ab85f2380597496889
luoping2 <luoping2@sensetime.com>
2024-02-23 11:54:04 +0800

    delete extension/build

--- extension/build/conan-dependencies/toolchain.cmake
+++
@@ -1,4 +0,0 @@
-
-        set(CMAKE_C_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/cl.exe")
-        set(CMAKE_CXX_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/cl.exe")
-        
